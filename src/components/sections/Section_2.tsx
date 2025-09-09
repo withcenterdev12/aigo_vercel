0fbf9cb2bf7100aeafc15a1a07b0565154750c12
Yves <withcenter.dev12@gmail.com>
2025-09-09 16:28:22 +0800

    WIP

--- src/components/sections/Section_2.tsx
+++ src/components/sections/Section_2.tsx
@@ -1,6 +1,6 @@
 export default function Section_2() {
   return (
-    <section className="flex min-h-[660px] w-full flex-col overflow-hidden px-[16px] sm:min-h-[1220px] sm:w-[960px]">
+    <section className="flex min-h-[660px] w-full flex-col overflow-hidden px-[16px] sm:min-h-[1220px] sm:w-[990px]">
       {/* Mobile View Section Header */}
       <h2 className="text-mobile-28b-emphasis-title text-on-bg-a-9 mt-[50px] block sm:hidden">
         AI 견적
@@ -30,15 +30,15 @@ export default function Section_2() {
         <div className="relative mt-[60px] hidden h-[800px] w-full overflow-hidden sm:block">
           <img
             src="/images/showcase_ipad.png"
-            className="absolute right-[5%] z-20 h-auto w-[620px]"
+            className="absolute right-[7%] z-20 h-auto w-[620px]"
           />
           <img
             src="/images/slant_ipad.png"
-            className="absolute top-[38.75%] left-[10.25%] z-10 h-auto w-[446.42px]"
+            className="absolute top-[38.75%] left-[8.25%] z-10 h-auto w-[446.42px]"
           />
           <img
             src="/images/shadow.png"
-            className="absolute top-[55%] left-[6.5%] z-0 h-auto w-[494.11px]"
+            className="absolute top-[55%] left-[4.5%] z-0 h-auto w-[494.11px]"
           />
           <p className="text-pc-16r-subtext text-on-surface-a-5 absolute bottom-[5%] left-1/2 -translate-x-1/2">
             *에이고는 라이트 모드와 다크 모드를 지원합니다.
